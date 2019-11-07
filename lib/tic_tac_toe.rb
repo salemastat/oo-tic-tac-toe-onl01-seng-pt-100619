1da561a2ca6627de24719e2c4e3caee37b3ffcbb
Salem Asrat <salemasrat@gmail.com>
2019-11-07 21:36:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,3 +35,15 @@ end
   board[index]== "X" || board[index] == "O"
  end
  
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
