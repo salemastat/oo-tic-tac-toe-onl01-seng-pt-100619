fe6c57918c40638c40b64cfcde7e4d7d7154823a
Salem Asrat <salemasrat@gmail.com>
2019-11-07 21:51:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,64 +1,92 @@
-class TicTacToe 
-
- WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [6,4,2]
-]
-
- def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
- def input_to_index(user_input)
-  user_input.to_i - 1
- end
-
- def move(board, index, player)
-  board[index] = player
- end
- 
- def position_taken?(board, index)
-  board[index]== "X" || board[index] == "O"
- end
- 
- def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
- 
-def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  if valid_move?(board, index)
-    move(board, index, current_player(board))
-    display_board(board)
-  else
-    turn(board)
+class TicTacToe
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  def initialize()
+    @board =  Array.new(9," ")
   end
-end
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+  def move(index, current_player)
+    @board[index] = current_player
+  end
+  def position_taken?(location)
+    @board[location] != " " && @board[location] != ""
+  end
 
-def turn_count(board)
-  board.count{|token| token == "X" || token == "O"}
-end
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index,current_player)
+      display_board
+    else
+      turn
+    end
+  end
+  def turn_count
+    turn_count = 0
+    @board.each do |slot|
+      if slot=="X" || slot=="O"
+        turn_count+=1
+      end
+    end
+    turn_count
+  end
+  def current_player
+    turn_count%2==0 ? "X" : "O"
+  end
+  def won?
+    odp = WIN_COMBINATIONS.detect do |comb|
+      @board[comb[0]]=="X" && @board[comb[1]]=="X" && @board[comb[2]]=="X" || @board[comb[0]]=="O" && @board[comb[1]]=="O" && @board[comb[2]]=="O"
+        end
+  odp ? odp : false
+  end
+  def full?
+    @board.all?{|slot| slot == "X" || slot == "O"}? true : false
+  end
+  def draw?
+    won? ? false : full? ? true : false
+  end
+  def over?
+    won? || draw? || full?
+  end
+  def winner
+    win = won?
+    if(!win)
+      nil
+    else
+      @board[win[0]]
+    end
+  end
+  def play
+    until over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
 
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
-end
-
-def won?(board)
-  WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] &&
-    board[combo[1]] == board[combo[2]] &&
-    position_taken?(board, combo[0])
   end
 end
