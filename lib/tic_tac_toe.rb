4cb049c0f0724d34cc1c1dbeced7eb7c40933610
Salem Asrat <salemasrat@gmail.com>
2019-11-07 21:48:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,16 +4,16 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
-  WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [6,4,2]
-  ]
+ WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
  def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
